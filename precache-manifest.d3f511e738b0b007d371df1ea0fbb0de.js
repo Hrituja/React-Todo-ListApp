53ecdcb827b64660044990a7d9e12f744a6d3294
Hrituja <hrkhatavkar@gmail.com>
2020-06-16 22:12:17 +0530

    Updates

---
+++ precache-manifest.d3f511e738b0b007d371df1ea0fbb0de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1313aa7baac8be0d54465056470863d",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "1114cc6de5cdefdad2f8",
+    "url": "/React-Todo/static/css/main.9e7d2f9b.chunk.css"
+  },
+  {
+    "revision": "d99e26b6b22d1ff5a136",
+    "url": "/React-Todo/static/js/2.3ff8f0d5.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/React-Todo/static/js/2.3ff8f0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1114cc6de5cdefdad2f8",
+    "url": "/React-Todo/static/js/main.5caca4fa.chunk.js"
+  },
+  {
+    "revision": "fa82ec386654e4c9b3b3",
+    "url": "/React-Todo/static/js/runtime-main.d1e81e00.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
